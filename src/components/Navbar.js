f374cf0f5e24179cfdb9a472aa32707b93d94778
Alexandros Polymeros <alpolymer@gmail.com>
2023-01-14 18:11:48 +0200

    Initial

---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import logo from "../images/logo.svg";
+import { pageLinks, sociaLinks } from "./data";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a rel="noreferrer" href={link.href} className="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          {sociaLinks.map((link) => {
+            const { id, href, icon } = link;
+            return (
+              <li key={id}>
+                <a
+                  rel="noreferrer"
+                  href={href}
+                  target="_blank"
+                  className="nav-icon"
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+}
